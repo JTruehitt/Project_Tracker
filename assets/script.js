d0563f99c14b4b95415d610cc449f0839cedc01d
JTrue <truehitt@gmail.com>
2023-03-18 00:08:34 -0400

    added local storage saving and system to index projects to allow for saving and displaying multiple

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// setting global variables
 let timeDisplay = $(".timeDisplay");
 let currentDateDisplay = $(".currentDate");
 let currentTimeDisplay = $(".currentTime");
@@ -11,11 +12,13 @@ let formSubmitBtn = $(".submitBtn");
 currentDateDisplay.text(currentDate);
 currentTimeDisplay.text(currentTime);
 
+// updating time each second
 function updateTime() {
   currentTimeDisplay.text(dayjs().format("h:mm:ss a"));
 }
 setInterval(updateTime, 1000);
 
+// setting functionality of datepicker
 $(function () {
   $("#datepicker").datepicker({
     changeMonth: true,
@@ -25,12 +28,24 @@ $(function () {
   });
 });
 
+// on form submit, sets project number which will act as the key to save project, and will iterate this number to allow to save different projects in storage
+// grabs values from form inputs, adds them to projectDetails object, and sends them to local storage
 formSubmitBtn.click(function () {
+  let projectNumber = localStorage.getItem("projectNumber");
+  if (projectNumber === null) {
+    projectNumber = 1;
+  }
+
   let projectName = projectNameInput.val();
   let projectType = projectTypeInput.val();
   let dueDate = dueDateInput.val();
 
-  console.log(projectName);
-  console.log(projectType);
-  console.log(dueDate);
+  let projectDetails = { projectName, projectType, dueDate };
+
+  localStorage.setItem(projectNumber, JSON.stringify(projectDetails));
+
+  projectNumber++;
+  localStorage.setItem("projectNumber", projectNumber);
 });
+
+// localStorage.clear()
